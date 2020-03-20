9c1c89d9a0fabc78ebf08c3f875b4a2b26593ff5
JRAM937 <43487925+JRAM937@users.noreply.github.com>
2020-03-20 16:25:30 -0700

    Update and rename Account to Account.java

--- Account.java
+++ Account.java
@@ -1,14 +1,15 @@
 package hw5;
 
-public class Account implements Withdrawable, Depositable, Balanceable{
-	
+public class Account implements Withdrawable,
+Depositable,
+Balanceable {
+
 	private double balance;
 	private int transactions = 0;
-	
+
 	public int getTrans() {
 		return this.transactions;
 	}
-	
 
 	//Getter methoid for double "balance"
 	@Override
@@ -19,23 +20,23 @@ public class Account implements Withdrawable, Depositable, Balanceable{
 	//Setter method for double "balance"
 	@Override
 	public void setBalance(double amount) {
-		this.balance = amount;	
+		this.balance = amount;
 	}
 
-	//Takes monet from a specified double "amount"
+	//Takes money from a specified double "amount"
 	@Override
 	public boolean withdraw(double amount) {
 		double check = this.balance - amount;
-		
-		if(check > 0) {
+
+		if (check > 0) {
 			this.balance = check;
 			this.transactions++;
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	//Deposit into an account
 	@Override
 	public void deposit(double amount) {
