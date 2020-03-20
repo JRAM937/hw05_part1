90334c2a19d696f88b50465dd5fad05821cc3ec7
JRAM937 <43487925+JRAM937@users.noreply.github.com>
2020-03-20 16:27:04 -0700

    Update and rename SavingsAccount to SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,26 +1,23 @@
 package hw5;
-import java.lang.Math; 
 
-public class SavingsAccount extends Account{
+public class SavingsAccount extends Account {
 	private double interestRate; //Monthly intetest rate
-	@SuppressWarnings("unused")
 	private double interestPayment; //Last monthly payment
-	
+
 	//constructor using superclass
 	public SavingsAccount() {
 		super();
 		this.interestRate = 0.0;
 		this.interestPayment = 0.0;
 	}
-	
+
 	//constructor using parameters
 	public SavingsAccount(double balance, double rate) {
 		super();
 		this.setBalance(balance);
 		this.interestRate = rate;
 	}
-	
-	
+
 	public double getInterestRate() {
 		return interestRate;
 	}
@@ -38,11 +35,10 @@ public class SavingsAccount extends Account{
 	}
 
 	public double calcInterest() {
-		double payment = interestRate*this.getBalance();
+		double payment = interestRate * this.getBalance();
 		this.setInterestPayment(payment);
 		this.setBalance(this.getBalance() + payment);
 		return this.interestPayment;
 	}
-	
-}
 
+}
